d70c006bca9e4ccf5ffc6735a40726910fcd013c
DylanBlanco <dylanblanco1198@gmail.com>
2024-07-17 19:47:34 +0200

    MILESTONE2 - carica img dinamicamente

--- js/script.js
+++ js/script.js
@@ -1,26 +1,35 @@
+/*
 //Array
-const img = ["img01","img02","img03","img04","img05"];
+const imgs = ["img/01.webp","img/02.webp","img/03.webp","img/04.webp","img/05.webp"];
 
-for (let i = 0;i < img.length; i++) {
-    console.log (img[i]);
+for (let i = 0;i < imgs.length; i++) {
+    console.log (imgs[i]);
 }
 
-//template literal
-const item = document.querySelector (`.box-img`);
+//ciclo for con template literal
+const item = document.querySelector(`.box-img`);
+console.log('primo selettore:', item)
 
-for (let i = 0; i < img.length; i++) {
+for (let i = 0; i < imgs.length; i++) {
     const img = document.createElement('img');
-    img.src = img[i];
+    imgs.src = imgs[i];
     img.classList.add('image');
 
+    console.log('il tag creato è: ', img)
+    
     if (i !== 0) {
-        img.style.display = 'none';
+        imgs.style.display = 'none';
     }
+
+    // (condizione) ? "se è vero" : "se è falso"
+    // i !== 0 ? "nome classe"
+
+    console.log( item);
     item.append(img);
 
     item.innerHTML += `
-        <div class="box-img img-block">
-            <img src="${`img/01.webp`}">
+        <div class="box-imgs imgs-block ${ (i !== 0 ? ... : ...i.)}">
+            <img src="${}">
                 <div class="button-right">
                     <button><i class="fa-solid fa-circle-chevron-right"></i></button>
                 </div>
@@ -29,4 +38,25 @@ for (let i = 0; i < img.length; i++) {
                 </div>
         </div>
     `;
+}
+    */
+// Array
+const imgs = ["img/01.webp","img/02.webp","img/03.webp","img/04.webp","img/05.webp"];
+for (let i = 0;i < imgs.length; i++) {
+    console.log (imgs[i]);
+}
+
+//selezione classe dell'html
+const boxImg = document.querySelector(`.box-img`);
+console.log (boxImg);
+
+//carica img dinamicamente
+for (let i = 0; i < imgs.length; i++) {
+    const img = document.createElement(`img`);
+    img.src = imgs[i];
+    img.classList.add(`img`);
+    if (i != 0) {
+        img.style.display = `none`;
+    }
+    boxImg.append(img);
 }
